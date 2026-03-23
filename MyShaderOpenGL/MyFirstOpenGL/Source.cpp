c4ce0f738a297b8febf95f175727af4c602bf78c
Edgar Novas <edgar.novas@enti.cat>
2026-03-23 15:50:05 +0100

    Shader Upload

--- MyShaderOpenGL/MyFirstOpenGL/Source.cpp
+++ MyShaderOpenGL/MyFirstOpenGL/Source.cpp
@@ -3,9 +3,16 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
+
+struct ShaderProgram
+{
+	GLuint vertexShader = 0;
+
+};
 
 void Resize_Window(GLFWwindow* window, int iFrameBufferWidth, int iFrameBufferHeight) {
 
@@ -39,6 +46,93 @@ std::string Load_File(const std::string& filePath)
 	return fileContent;
 }
 
+GLuint LoadVertexShader(const std::string& filePath)
+{
+	//Crear vertex shader a la GPU
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	//Leo el archivo con el shader y lo almaceno
+	std::string sShaderCode = Load_File(filePath);
+	const char* cShaderSource = sShaderCode.c_str();
+
+	//Vinculo el vertex shader a la GPU
+	glShaderSource(vertexShader, 1, &cShaderSource, nullptr);
+
+	//Compilar vertex shader
+	glCompileShader(vertexShader);
+
+	//Verificacion de la compilacion del shader
+	GLint success;
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (success)
+	{
+		return vertexShader;
+	}
+	else
+	{
+		//Obtener longitud del log
+		GLint logLength;
+		glGetShaderiv(vertexShader,GL_INFO_LOG_LENGTH,&logLength);
+
+		//Obtenemos el log
+		std::vector<GLchar> errorLog(logLength);
+		glGetShaderInfoLog(vertexShader, logLength, nullptr, errorLog.data() );
+
+		//Mostramos el log
+		std::cerr << "Se ha producido el siguiente error : " << errorLog.data() << std::endl;
+
+		std::exit(EXIT_FAILURE);
+
+	}
+
+}
+
+//Usando el struct encapsula los shaders creare el programa en la GPU que los usara
+GLuint CreateProgram(const ShaderProgram& shaders)
+{
+	//Creamos programa
+	GLuint program = glCreateProgram();
+
+	//Verificar si existe un vertex Shader a cargar
+	if (shaders.vertexShader != 0)
+	{
+		glAttachShader(program,shaders.vertexShader);
+	}
+	
+	//Linkear el programa
+	glLinkProgram(program);
+
+	//Obtener estado del programa
+	GLint success;
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+	if (success)
+	{
+		//Liberar recursos
+
+		if (shaders.vertexShader != 0)
+		{
+			glDetachShader(program, shaders.vertexShader);
+		}
+
+		return program;
+	}
+	else
+	{
+		//Obtenemos la longitud del log
+		GLint logLength;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
+
+		//almacenamos log
+		std::vector <GLchar> errorLog(logLength);
+		glGetProgramInfoLog(program, logLength, nullptr, errorLog.data());
+
+		//Printeamos log
+		std::cerr << "Error al linkar programa: " << errorLog.data() << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
+}
 
 void main()
 {
@@ -47,6 +141,7 @@ void main()
 	//Inicializamos GLFW para gestionar ventanas e inputs
 	glfwInit();
 
+	
 	//Configuramos la ventana
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -73,6 +168,14 @@ void main()
 
 	//Inicializamos GLEW y controlamos errores
 	if (glewInit() == GLEW_OK) {
+
+		//Compilar shaders
+		ShaderProgram myFirstProgram;
+		myFirstProgram.vertexShader = LoadVertexShader("MyFirstVertexShader.glsl");
+
+		//Compilar programa
+		GLuint myFirstCompiledProgram;
+		myFirstCompiledProgram = CreateProgram(myFirstProgram);
 
 		//Definimos color para limpiar el buffer de color
 		glClearColor(1.f, 0.f, 0.f, 1.f);
