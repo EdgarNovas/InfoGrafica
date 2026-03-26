865f59c4168ff467909281eda17a500c190b0ede
Edgar Novas <edgar.novas@enti.cat>
2026-03-26 15:48:40 +0100

    Geometry Shader

--- MyShaderOpenGL/MyFirstOpenGL/Source.cpp
+++ MyShaderOpenGL/MyFirstOpenGL/Source.cpp
@@ -12,6 +12,7 @@
 struct ShaderProgram
 {
 	GLuint vertexShader = 0;
+	GLuint geometryShader = 0;
 };
 
 void Resize_Window(GLFWwindow* window, int iFrameBufferWidth, int iFrameBufferHeight) {
@@ -44,6 +45,48 @@ std::string Load_File(const std::string& filePath)
 
 	file.close();
 	return fileContent;
+}
+
+GLuint LoadGeometryShader(const std::string& filePath) //Podriamos poner aqui un enum para recbir y saber si es geometry o vertex shader
+{
+	//Crear vertex shader a la GPU
+	GLuint geometryShader = glCreateShader(GL_GEOMETRY_SHADER);
+
+	//Leo el archivo con el shader y lo almaceno
+	std::string sShaderCode = Load_File(filePath);
+	const char* cShaderSource = sShaderCode.c_str();
+
+	//Vinculo el vertex shader a la GPU
+	glShaderSource(geometryShader, 1, &cShaderSource, nullptr);
+
+	//Compilar vertex shader
+	glCompileShader(geometryShader);
+
+	//Verificacion de la compilacion del shader
+	GLint success;
+	glGetShaderiv(geometryShader, GL_COMPILE_STATUS, &success);
+
+	if (success)
+	{
+		return geometryShader;
+	}
+	else
+	{
+		//Obtener longitud del log
+		GLint logLength;
+		glGetShaderiv(geometryShader, GL_INFO_LOG_LENGTH, &logLength);
+
+		//Obtenemos el log
+		std::vector<GLchar> errorLog(logLength);
+		glGetShaderInfoLog(geometryShader, logLength, nullptr, errorLog.data());
+
+		//Mostramos el log
+		std::cerr << "Se ha producido el siguiente error : " << errorLog.data() << std::endl;
+
+		std::exit(EXIT_FAILURE);
+
+	}
+
 }
 
 GLuint LoadVertexShader(const std::string& filePath)
@@ -100,6 +143,11 @@ GLuint CreateProgram(const ShaderProgram& shaders)
 		glAttachShader(program,shaders.vertexShader);
 	}
 	
+	if (shaders.geometryShader != 0)
+	{
+		glAttachShader(program, shaders.geometryShader);
+	}
+
 	//Linkear el programa
 	glLinkProgram(program);
 
@@ -114,6 +162,11 @@ GLuint CreateProgram(const ShaderProgram& shaders)
 		if (shaders.vertexShader != 0)
 		{
 			glDetachShader(program, shaders.vertexShader);
+		}
+
+		if (shaders.geometryShader != 0)
+		{
+			glDetachShader(program, shaders.geometryShader);
 		}
 
 		return program;
@@ -175,6 +228,7 @@ void main()
 		//Compilar shaders
 		ShaderProgram myFirstProgram;
 		myFirstProgram.vertexShader = LoadVertexShader("MyFirstVertexShader.glsl");
+		myFirstProgram.geometryShader = LoadGeometryShader("MyFirstGeometryShader.glsl");
 
 		//Compilar programa
 		GLuint myFirstCompiledProgram;
